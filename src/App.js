e2a65dedf38b8927f77e7071d068fad452ab223b
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-04-25 23:04:28 +0200

    moar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     console.log("Kebab case!: ", kebabCase);
     console.log("Kebab case!2: ", kebabCase);
     console.log("Kebab case!3: ", kebabCase);
+    console.log("Kebab case!3: ", kebabCase);
     alert("AAAAAAAA this button does nothing!");
   };
 
