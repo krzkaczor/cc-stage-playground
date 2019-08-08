f6dbdbf10de7d1b37745f4b8b5348a7323cc5bab
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-09 00:12:26 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { kebabCase } from "lodash";
 
 class App extends Component {
   onClick = () => {
